14a9e379e380e3d283978c5ac23acdce7fdf1f0d
icebery425 <zhihu_425@163.com>
2018-04-13 00:26:55 +0800

    fix bug

--- app/src/main/java/com/jinglangtech/teamchat/MeizuTestReceiver.java
+++ app/src/main/java/com/jinglangtech/teamchat/MeizuTestReceiver.java
@@ -8,7 +8,7 @@ import com.umeng.message.meizu.UmengMeizuPushReceiver;
  * Created by Administrator on 2018-04-11.
  */
 
-public class JltMeizuPushReceiver extends UmengMeizuPushReceiver {
+public class MeizuTestReceiver extends UmengMeizuPushReceiver {
 
     @Override
     public void onMessage(Context context, String s) {
